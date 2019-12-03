30c81a3853010a18b29edbd965b7149d333cbefc
Zorro-R <jurijlwaite@gmail.com>
2019-12-03 22:27:17 +0100

    Added solutions to Stackoverflow post exercise

---
+++ Stackoverflow Post/Stackoverflow Post/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Stackoverflow_Post
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public DateTime PostDate { get; private set; }
+        public int Votes { get; private set; }
+
+        public Post()
+        {
+            PostDate = DateTime.Now;
+            Votes = 0;
+        }
+
+        public Post(string title)
+            :this()
+        {
+            Title = title;
+        }
+
+        public void Upvote()
+        {
+            Votes++;
+        }
+
+        public void Downvote()
+        {
+            Votes--;
+        }
+    }
+}
---
+++ Stackoverflow Post/Stackoverflow Post/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stackoverflow_Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post("Test Post Pls Ignore");
+            Console.WriteLine(post.Votes);
+            Console.WriteLine(post.PostDate);
+            Console.WriteLine(post.Title);
+            post.Upvote();
+            post.Downvote();
+            post.Upvote();
+            post.Upvote();
+            post.Upvote();
+            Console.WriteLine(post.Votes);
+            Console.WriteLine(post.PostDate);
+            Console.WriteLine(post.Title);
+        }
+    }
+}
