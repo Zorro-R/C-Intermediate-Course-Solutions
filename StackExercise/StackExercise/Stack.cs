4d71b1711c6da439b2df0047c1103dbbc5bb5623
Zorro-R <jurijlwaite@gmail.com>
2019-12-03 22:25:44 +0100

    Added solutions to stack exercise

---
+++ StackExercise/StackExercise/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackExercise
+{
+    public class Stack
+    {
+        private List<object> _stackList;
+        public object topObj { get; private set; }
+        
+        public Stack()
+        {
+            _stackList = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException();
+            else
+            {
+                _stackList.Add(obj);
+            }
+        }
+
+        public object Pop()
+        {
+            if (_stackList.Count > 0)
+            {
+                topObj = _stackList[_stackList.Count - 1];
+                _stackList.RemoveAt(_stackList.Count - 1);
+                return topObj;
+            }
+            // In case the stack is empty we cannot call the .Pop() method
+            else
+                throw new InvalidOperationException();
+            
+        }
+
+        public void Clear()
+        {
+            _stackList.Clear();
+        }
+    }
+}
